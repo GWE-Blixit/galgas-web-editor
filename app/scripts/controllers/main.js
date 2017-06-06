5e88e407487cb78368f91c9e73b1dee17b550a5b
blixit <celestinya@gmail.com>
2017-06-06 20:30:20 +0200

    Installing font awesome and updating the view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,25 @@
  * Controller of the galgasWebEditorApp
  */
 app
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
+    $scope.projects = [
+      {
+        name : "Global",
+        description : "Create and manage your components "
+      },
+      {
+        name : "jsonlikeql",
+        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!"
+      },
+      {
+        name : "logo",
+        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!"
 
+      }
+    ];
+
+    $scope.init = function(){
+
+    }
 
   });

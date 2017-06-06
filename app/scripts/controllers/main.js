efecf3fa9b2f87c07455e10a64b5c69e681d7914
blixit <celestinya@gmail.com>
2017-06-06 20:44:00 +0200

    Main page done !

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,15 +12,18 @@ app
     $scope.projects = [
       {
         name : "Global",
-        description : "Create and manage your components "
+        description : "Create and manage your components ",
+        creation : (new Date()).toDateString()
       },
       {
         name : "jsonlikeql",
-        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!"
+        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!",
+        creation : (new Date()).toDateString()
       },
       {
         name : "logo",
-        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!"
+        description : " Here stands my project description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam asperiores, beatae blanditiis corporis culpa deserunt dicta dolore eaque est eum ipsum, labore maiores pariatur quas repellendus sed, similique vero!",
+        creation : (new Date()).toDateString()
 
       }
     ];
